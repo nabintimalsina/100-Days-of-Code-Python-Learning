061a37227226ab4c65c77d1b88cd76eab1b0f173
nabintimalsina <nabintimalsina66@gmail.com>
2026-08-09 10:05:42 +0545

    love project

---
+++ Function_Project/love_score.py
@@ -0,0 +1,15 @@
+def calculate_love_score():
+    true = input("Enter the name TRUE: \n")
+    love = input("Enter the name LOVE: \n")
+    
+    concate = true + love
+    print(f"Concatenate letter is : {concate}")
+    
+calculate_love_score()    
+    
+    
+    
+    
+    
+    
+    
--- test.py
+++
@@ -1,5 +0,0 @@
-def user_love():
-    print("I love you!")
-    print("I hate you")
-user_love()
-#This is done now
