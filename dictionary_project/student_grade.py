232b9cb928308d9983622068d3baa6252edfb9fd
nabintimalsina <nabintimalsina66@gmail.com>
2026-08-11 16:34:48 +0545

    created the dictionary project

---
+++ dictionary_project/student_grade.py
@@ -0,0 +1,20 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+sudent_grade = {}
+for student in student_scores:
+    
+        if student range(91,100):
+            print("Outstanding")
+        elif student range (81,90):
+            print("Exceeds Expectations")
+        elif student range(71,80):
+            print("Acceptable")
+        else:
+            print("Fail")
+            
